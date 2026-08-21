8f1a0503136d94709d8114d745e4f3d101ad5759
Alexander Verbruggen <alex@celerium.be>
2026-08-21 11:33:53 +0200

    Add aliases for all previously supported artifact types

--- src/main/java/be/nabu/maven/environment/ArtifactAliases.java
+++ src/main/java/be/nabu/maven/environment/ArtifactAliases.java
@@ -40,6 +40,54 @@ public final class ArtifactAliases {
 		else if ("be.nabu.eai.module.swagger.client.SwaggerClientManager".equals(artifactType)) {
 			swaggerClient(aliases);
 		}
+		else if ("be.nabu.eai.module.rest.client.RESTClientManager".equals(artifactType)) {
+			restClient(aliases);
+		}
+		else if ("be.nabu.eai.module.rest.provider.RESTEndpointManager".equals(artifactType)) {
+			restEndpoint(aliases);
+		}
+		else if ("be.nabu.eai.module.http.client.HTTPClientManager".equals(artifactType)) {
+			httpClient(aliases);
+		}
+		else if ("be.nabu.eai.module.smtp.SMTPClientManager".equals(artifactType)) {
+			smtpClient(aliases);
+		}
+		else if ("be.nabu.eai.module.misc.executor.ExecutorManager".equals(artifactType)) {
+			executor(aliases);
+		}
+		else if ("be.nabu.eai.module.misc.features.FeatureSetManager".equals(artifactType)) {
+			featureSet(aliases);
+		}
+		else if ("be.nabu.eai.module.web.application.WebApplicationManager".equals(artifactType)) {
+			webApplication(aliases);
+		}
+		else if ("be.nabu.eai.module.http.virtual.WAFManager".equals(artifactType)) {
+			waf(aliases);
+		}
+		else if ("be.nabu.eai.module.jwk.JWKManager".equals(artifactType)) {
+			jwk(aliases);
+		}
+		else if ("be.nabu.eai.module.misc.compressor.CompressorManager".equals(artifactType)) {
+			compressor(aliases);
+		}
+		else if ("be.nabu.eai.module.http.icap.ICAPVirusScannerManager".equals(artifactType)) {
+			icapVirusScanner(aliases);
+		}
+		else if ("be.nabu.eai.module.http.wiki.WikiManager".equals(artifactType)) {
+			wiki(aliases);
+		}
+		else if ("be.nabu.eai.module.odata.client.ODataClientManager".equals(artifactType)) {
+			oDataClient(aliases);
+		}
+		else if ("be.nabu.eai.module.hazelcast.HazelcastClusterManager".equals(artifactType)) {
+			hazelcastCluster(aliases);
+		}
+		else if ("be.nabu.eai.module.channel.ChannelManager".equals(artifactType)) {
+			channel(aliases);
+		}
+		else if ("be.nabu.eai.module.wsdl.client.WSDLClientManager".equals(artifactType)) {
+			wsdlClient(aliases);
+		}
 		return aliases;
 	}
 
@@ -96,6 +144,20 @@ public final class ArtifactAliases {
 		alias(aliases, "enableCompression", "virtual-host.xml", "/virtualHost/enableCompression/text()", false);
 	}
 
+	private static void restEndpoint(Map<String, AliasTarget> aliases) {
+		alias(aliases, "host", "rest-endpoint.xml", "/restEndpoint/host/text()", false);
+		alias(aliases, "basePath", "rest-endpoint.xml", "/restEndpoint/basePath/text()", false);
+		alias(aliases, "secure", "rest-endpoint.xml", "/restEndpoint/secure/text()", false);
+		alias(aliases, "httpClient", "rest-endpoint.xml", "/restEndpoint/httpClient/text()", false);
+		alias(aliases, "username", "rest-endpoint.xml", "/restEndpoint/username/text()", false);
+		alias(aliases, "password", "rest-endpoint.xml", "/restEndpoint/password/text()", false);
+		alias(aliases, "apiHeaderName", "rest-endpoint.xml", "/restEndpoint/apiHeaderName/text()", false);
+		alias(aliases, "apiQueryName", "rest-endpoint.xml", "/restEndpoint/apiQueryName/text()", false);
+		alias(aliases, "apiQueryKey", "rest-endpoint.xml", "/restEndpoint/apiQueryKey/text()", false);
+		alias(aliases, "apiHeaderKey", "rest-endpoint.xml", "/restEndpoint/apiHeaderKey/text()", false);
+		alias(aliases, "gzip", "rest-endpoint.xml", "/restEndpoint/gzip/text()", false);
+	}
+
 	private static void swaggerClient(Map<String, AliasTarget> aliases) {
 		alias(aliases, "host", "swagger-client.xml", "/swaggerClient/host/text()", false);
 		alias(aliases, "basePath", "swagger-client.xml", "/swaggerClient/basePath/text()", false);
@@ -111,6 +173,112 @@ public final class ArtifactAliases {
 		alias(aliases, "apiHeaderKey", "swagger-client.xml", "/swaggerClient/apiHeaderKey/text()", false);
 		alias(aliases, "bearerToken", "swagger-client.xml", "/swaggerClient/bearerToken/text()", false);
 		alias(aliases, "supportGzip", "swagger-client.xml", "/swaggerClient/supportGzip/text()", false);
+	}
+
+	private static void restClient(Map<String, AliasTarget> aliases) {
+		alias(aliases, "host", "rest-client.xml", "/restClient/host/text()", false);
+		alias(aliases, "path", "rest-client.xml", "/restClient/path/text()", false);
+		alias(aliases, "secure", "rest-client.xml", "/restClient/secure/text()", false);
+		alias(aliases, "httpClient", "rest-client.xml", "/restClient/httpClient/text()", false);
+		alias(aliases, "username", "rest-client.xml", "/restClient/username/text()", false);
+		alias(aliases, "password", "rest-client.xml", "/restClient/password/text()", false);
+		alias(aliases, "gzip", "rest-client.xml", "/restClient/gzip/text()", false);
+	}
+
+	private static void httpClient(Map<String, AliasTarget> aliases) {
+		alias(aliases, "type", "http-client.xml", "/httpClient/type/text()", false);
+		alias(aliases, "static", "http-client.xml", "/httpClient/static/text()", false);
+		alias(aliases, "keystore", "http-client.xml", "/httpClient/keystore/text()", false);
+		alias(aliases, "sslContextType", "http-client.xml", "/httpClient/sslContextType/text()", false);
+		alias(aliases, "captureErrors", "http-client.xml", "/httpClient/captureErrors/text()", false);
+		alias(aliases, "captureSuccessful", "http-client.xml", "/httpClient/captureSuccessful/text()", false);
+		alias(aliases, "ioPoolSize", "http-client.xml", "/httpClient/ioPoolSize/text()", false);
+		alias(aliases, "processPoolSize", "http-client.xml", "/httpClient/processPoolSize/text()", false);
+		alias(aliases, "connectionTimeout", "http-client.xml", "/httpClient/connectionTimeout/text()", false);
+		alias(aliases, "socketTimeout", "http-client.xml", "/httpClient/socketTimeout/text()", false);
+		alias(aliases, "maxAmountOfConnectionsPerTarget", "http-client.xml", "/httpClient/maxAmountOfConnectionsPerTarget/text()", false);
+	}
+
+	private static void smtpClient(Map<String, AliasTarget> aliases) {
+		alias(aliases, "host", "smtp-server.xml", "/smtpClient/host/text()", false);
+		alias(aliases, "port", "smtp-server.xml", "/smtpClient/port/text()", false);
+		alias(aliases, "from", "smtp-server.xml", "/smtpClient/from/text()", false);
+		alias(aliases, "subjectTemplate", "smtp-server.xml", "/smtpClient/subjectTemplate/text()", false);
+		alias(aliases, "clientHost", "smtp-server.xml", "/smtpClient/clientHost/text()", false);
+		alias(aliases, "charset", "smtp-server.xml", "/smtpClient/charset/text()", false);
+		alias(aliases, "username", "smtp-server.xml", "/smtpClient/username/text()", false);
+		alias(aliases, "password", "smtp-server.xml", "/smtpClient/password/text()", true);
+		alias(aliases, "loginMethod", "smtp-server.xml", "/smtpClient/loginMethod/text()", false);
+		alias(aliases, "implicitSSL", "smtp-server.xml", "/smtpClient/implicitSSL/text()", false);
+		alias(aliases, "startTls", "smtp-server.xml", "/smtpClient/startTls/text()", false);
+		alias(aliases, "keystore", "smtp-server.xml", "/smtpClient/keystore/text()", false);
+		alias(aliases, "blacklist", "smtp-server.xml", "/smtpClient/blacklist/text()", false);
+		alias(aliases, "overrideToInMime", "smtp-server.xml", "/smtpClient/overrideToInMime/text()", false);
+		alias(aliases, "connectionTimeout", "smtp-server.xml", "/smtpClient/connectionTimeout/text()", false);
+		alias(aliases, "socketTimeout", "smtp-server.xml", "/smtpClient/socketTimeout/text()", false);
+	}
+
+	private static void executor(Map<String, AliasTarget> aliases) {
+		alias(aliases, "poolSize", "executor.xml", "/executor/poolSize/text()", false);
+	}
+
+	private static void featureSet(Map<String, AliasTarget> aliases) {
+	}
+
+	private static void webApplication(Map<String, AliasTarget> aliases) {
+		alias(aliases, "virtualHost", "webartifact.xml", "/webartifact/virtualHost/text()", false);
+		alias(aliases, "path", "webartifact.xml", "/webartifact/path/text()", false);
+	}
+
+	private static void waf(Map<String, AliasTarget> aliases) {
+		alias(aliases, "baseHost", "waf.xml", "/waf/baseHost/text()", false);
+		alias(aliases, "server", "waf.xml", "/waf/server/text()", false);
+		alias(aliases, "redirectServer", "waf.xml", "/waf/redirectServer/text()", false);
+	}
+
+	private static void jwk(Map<String, AliasTarget> aliases) {
+	}
+
+	private static void compressor(Map<String, AliasTarget> aliases) {
+		alias(aliases, "enabled", "compressor.xml", "/compressor/enabled/text()", false);
+		alias(aliases, "charset", "compressor.xml", "/compressor/charset/text()", false);
+	}
+
+	private static void icapVirusScanner(Map<String, AliasTarget> aliases) {
+		alias(aliases, "host", "virus-scanner.xml", "/virusScanner/host/text()", false);
+		alias(aliases, "path", "virus-scanner.xml", "/virusScanner/path/text()", false);
+		alias(aliases, "secure", "virus-scanner.xml", "/virusScanner/secure/text()", false);
+		alias(aliases, "keystore", "virus-scanner.xml", "/virusScanner/keystore/text()", false);
+	}
+
+	private static void wiki(Map<String, AliasTarget> aliases) {
+		alias(aliases, "source", "wiki.xml", "/wiki/source/text()", false);
+		alias(aliases, "charset", "wiki.xml", "/wiki/charset/text()", false);
+	}
+
+	private static void oDataClient(Map<String, AliasTarget> aliases) {
+		alias(aliases, "endpoint", "odata-client.xml", "/odataClient/endpoint/text()", false);
+		alias(aliases, "securityType", "odata-client.xml", "/odataClient/securityType/text()", false);
+		alias(aliases, "securityContext", "odata-client.xml", "/odataClient/securityContext/text()", false);
+	}
+
+	private static void hazelcastCluster(Map<String, AliasTarget> aliases) {
+		alias(aliases, "port", "hazelcast-cluster.xml", "/hazelcastCluster/port/text()", false);
+		alias(aliases, "hazelcastPort", "hazelcast-cluster.xml", "/hazelcastCluster/hazelcastPort/text()", false);
+		alias(aliases, "amazonTagKey", "hazelcast-cluster.xml", "/hazelcastCluster/amazonTagKey/text()", false);
+		alias(aliases, "amazonTagValue", "hazelcast-cluster.xml", "/hazelcastCluster/amazonTagValue/text()", false);
+		alias(aliases, "amazonRegion", "hazelcast-cluster.xml", "/hazelcastCluster/amazonRegion/text()", false);
+	}
+
+	private static void channel(Map<String, AliasTarget> aliases) {
+	}
+
+	private static void wsdlClient(Map<String, AliasTarget> aliases) {
+		alias(aliases, "endpoint", "wsdl-client.xml", "/wsdlClient/endpoint/text()", false);
+		alias(aliases, "charset", "wsdl-client.xml", "/wsdlClient/charset/text()", false);
+		alias(aliases, "httpClient", "wsdl-client.xml", "/wsdlClient/httpClient/text()", false);
+		alias(aliases, "username", "wsdl-client.xml", "/wsdlClient/username/text()", false);
+		alias(aliases, "password", "wsdl-client.xml", "/wsdlClient/password/text()", false);
 	}
 
 	private static void alias(Map<String, AliasTarget> aliases, String alias, String fileName, String query, boolean encrypted) {
